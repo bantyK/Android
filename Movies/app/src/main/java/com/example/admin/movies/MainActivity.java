4647728bc16f8a6e5b86d954a6311c816e6a8d35
Banty Kumar <bantyp92@gmail.com>
2017-02-13 07:58:06 -0800

    1. Logic added to from url from movie name(movie name hardcoded)

--- Movies/app/src/main/java/com/example/admin/movies/MainActivity.java
+++ Movies/app/src/main/java/com/example/admin/movies/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.admin.movies;
 
+import android.net.Network;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String movieName = "The Avengers"; // will be taken from the user
+        URL targetURL = NetworkUtils.formQueryURL(movieName);
     }
 }
---
+++ Movies/app/src/main/java/com/example/admin/movies/NetworkUtils.java
@@ -0,0 +1,43 @@
+package com.example.admin.movies;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+/**
+ * Created by Banty on 2/13/2017.
+ */
+
+public class NetworkUtils {
+
+    private static final String TAG = NetworkUtils.class.getSimpleName();
+    public static final String BASE_URL = "api.themoviedb.org";
+    public static final String API_KEY = "b33cef0e8f9a26e8857366681a641e25";
+    public static URL formQueryURL(String movieName){
+        Uri.Builder uriBuilder = new Uri.Builder();
+        uriBuilder.scheme("https")
+                .authority(BASE_URL)
+                .appendPath("3")
+                .appendPath("search")
+                .appendPath("movie")
+                .appendQueryParameter("api_key",API_KEY)
+                .appendQueryParameter("query",movieName);
+
+        String targetUrlString = uriBuilder.build().toString();
+        Log.i(TAG, "formQueryURL: Movie URL : " + targetUrlString);
+
+        URL targetUrl = null;
+
+        try{
+            targetUrl = new URL(targetUrlString);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return targetUrl;
+
+
+    }
+}
