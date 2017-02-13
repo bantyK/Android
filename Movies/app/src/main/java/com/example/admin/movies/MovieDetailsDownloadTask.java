9353126044e3bfc8e993a9056e989a7c3d6eb018
Banty Kumar <bantyp92@gmail.com>
2017-02-13 07:34:36 -0800

    1. Movie app project added 2. AsyncTask added for downloading the json from tmdb api

---
+++ Movies/app/src/main/java/com/example/admin/movies/MovieDetailsDownloadTask.java
@@ -0,0 +1,17 @@
+package com.example.admin.movies;
+
+import android.os.AsyncTask;
+
+import java.net.URL;
+
+/**
+ * Created by Banty on 2/13/2017.
+ */
+
+public class MovieDetailsDownloadTask extends AsyncTask<URL,Void,String> {
+
+    @Override
+    protected String doInBackground(URL... params) {
+        return null;
+    }
+}
