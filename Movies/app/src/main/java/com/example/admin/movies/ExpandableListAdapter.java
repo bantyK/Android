0bcfcd8a601a37975504277dca1e25346435f8a5
kumarbanty <banty.kumar@vuclip.com>
2017-02-19 22:12:56 +0530

    1. Movie detail UI added 2. Expandable list view for genre and production companies added

---
+++ Movies/app/src/main/java/com/example/admin/movies/ExpandableListAdapter.java
@@ -0,0 +1,90 @@
+package com.example.admin.movies;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class ExpandableListAdapter extends BaseExpandableListAdapter {
+
+    private Context mContext;
+    private List<String> listDataHeader;
+    private HashMap<String, List<String>> listDataChild;
+
+    public ExpandableListAdapter(Context mContext, List<String> listDataHeader, HashMap<String, List<String>> listDataChild) {
+        this.mContext = mContext;
+        this.listDataHeader = listDataHeader;
+        this.listDataChild = listDataChild;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return listDataHeader.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return listDataChild.get(this.listDataHeader.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return this.listDataHeader.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return this.listDataChild.get(this.listDataHeader.get(groupPosition)).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        final String headerTitle = (String) getGroup(groupPosition);
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) this.mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.list_group, parent, false);
+
+        }
+        TextView expandableListHeaderName = (TextView) convertView.findViewById(R.id.tv_expandable_list_header);
+        expandableListHeaderName.setText(headerTitle);
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        final String childText = (String) getChild(groupPosition, childPosition);
+
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) this.mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.exandable_list_item, parent, false);
+        }
+        TextView movieGenreName = (TextView) convertView.findViewById(R.id.tv_movie_genre_name);
+        movieGenreName.setText(childText);
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
+    }
+}
--- Movies/app/src/main/java/com/example/admin/movies/MovieDetailActivity.java
+++ Movies/app/src/main/java/com/example/admin/movies/MovieDetailActivity.java
@@ -5,20 +5,26 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by Banty on 2/14/2017.
  */
 
-public class MovieDetailActivity extends AppCompatActivity implements DownloadTaskCompletedListener{
+public class MovieDetailActivity extends AppCompatActivity implements DownloadTaskCompletedListener {
     private final static String TAG = MovieDetailActivity.class.getSimpleName();
-    private TextView movieTitleTextView;
+    private TextView movieTitleTextView, movieDetailCertificate, movieDetailTagline, movieDetailRating, movieDetailBudget, movieDetailRevenue;
     private ImageView movieThumbImageView;
+    private ExpandableListView expandableListView;
     private int movieId;
+    private ExpandableListAdapter expandableListAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -27,12 +33,20 @@ public class MovieDetailActivity extends AppCompatActivity implements DownloadTa
         setContentView(R.layout.movie_detail_activity);
 
         movieTitleTextView = (TextView) findViewById(R.id.tv_movie_detail_title);
+        movieDetailCertificate = (TextView) findViewById(R.id.tv_movie_detail_certificate);
+        movieDetailTagline = (TextView) findViewById(R.id.tv_movie_detail_tagline);
+        movieDetailRating = (TextView) findViewById(R.id.tv_movie_detail_rating);
+        movieDetailBudget = (TextView) findViewById(R.id.tv_movie_detail_budget);
+        movieDetailRevenue = (TextView) findViewById(R.id.tv_movie_detail_revenue);
         movieThumbImageView = (ImageView) findViewById(R.id.iv_movie_detail_thumb);
+        expandableListView = (ExpandableListView) findViewById(R.id.lv_expandable);
+
         Intent intent = getIntent();
-        if(intent != null && intent.hasExtra(IntentExtras.MOVIE_ID)){
+        if (intent != null && intent.hasExtra(IntentExtras.MOVIE_ID)) {
             movieId = intent.getExtras().getInt(IntentExtras.MOVIE_ID);
             URL movieDetailURL = new NetworkUtils().buildSingleMovieDetailURL(String.valueOf(movieId));
-            DownloaderTask downloaderTask = new DownloaderTask(this,this);
+            Log.d(TAG, "onCreate: http## : " + movieDetailURL);
+            DownloaderTask downloaderTask = new DownloaderTask(this, this);
             downloaderTask.execute(movieDetailURL);
         }
     }
@@ -42,9 +56,38 @@ public class MovieDetailActivity extends AppCompatActivity implements DownloadTa
         JsonParser jsonParser = new JsonParser();
         MovieDetail movieDetail = jsonParser.parseMovieDetails(jsonResponse);
         // TODO: 16/02/17 check for null movieDetail object while populating UI
-        movieTitleTextView.setText(movieDetail.getTitle());
-        URL thumbURL = new NetworkUtils().buildThumbURL(MovieSingleton.getInstance().getConfig(), movieDetail.getPosterPath());
-        ImageLoader.loadImageWithGlide(this, thumbURL.toString(), movieThumbImageView);
+        if (movieDetail != null) {
+            movieTitleTextView.setText(movieDetail.getTitle());
+//            movieDetailCertificate.setText(movieDetail.isAdult());
+            movieDetailTagline.setText(movieDetail.getTagline());
+            movieDetailRating.setText(String.valueOf(movieDetail.getAverageRating()));
+
+            if (movieDetail.getBudget().equals("0")) {
+                movieDetailBudget.append(getString(R.string.not_available));
+            } else {
+                movieDetailBudget.append(String.valueOf(movieDetail.getBudget()));
+            }
+
+            if (movieDetail.getRevenue().equals("0")) {
+                movieDetailRevenue.append(getString(R.string.not_available));
+            } else {
+                movieDetailRevenue.append(String.valueOf(movieDetail.getRevenue()));
+            }
+
+
+            URL thumbURL = new NetworkUtils().buildThumbURL(MovieSingleton.getInstance().getConfig(), movieDetail.getPosterPath());
+            ImageLoader.loadImageWithGlide(this, thumbURL.toString(), movieThumbImageView);
+
+
+            List<String> expandableListHeaders = new ArrayList<>();
+            expandableListHeaders.add(getString(R.string.expandable_list_header_genre));
+            expandableListHeaders.add(getString(R.string.expandable_list_header_prod_companies));
+            HashMap<String, List<String>> expandableListChildItems = new HashMap<>();
+            expandableListChildItems.put(expandableListHeaders.get(0), movieDetail.getGenres());
+            expandableListChildItems.put(expandableListHeaders.get(1), movieDetail.getProductionCompanies());
+            expandableListAdapter = new ExpandableListAdapter(this, expandableListHeaders, expandableListChildItems);
+            expandableListView.setAdapter(expandableListAdapter);
+        }
 
     }
 }
