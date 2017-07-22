8c63eb39a923f794430d28ad2e0860ed609cbe3f
kumarbanty <banty.kumar@vuclip.com>
2017-07-23 00:46:08 +0530

    Account activity in progress

---
+++ Instagramclone/app/src/main/java/example/banty/com/instagramclone/activities/profile/AccountSettingActivity.java
@@ -0,0 +1,14 @@
+package example.banty.com.instagramclone.activities.profile;
+
+import android.os.Bundle;
+
+import example.banty.com.instagramclone.R;
+import example.banty.com.instagramclone.activities.BaseActivity;
+
+public class AccountSettingActivity extends BaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_setting);
+    }
+}
--- Instagramclone/app/src/main/java/example/banty/com/instagramclone/activities/profile/ProfileActivity.java
+++ Instagramclone/app/src/main/java/example/banty/com/instagramclone/activities/profile/ProfileActivity.java
@@ -1,10 +1,13 @@
 package example.banty.com.instagramclone.activities.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
@@ -23,32 +26,22 @@ public class ProfileActivity extends BaseActivity {
         setContentView(R.layout.activity_profile);
         BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottom_nav_view_bar);
         setupBottomNavigationView(bottomNavigationViewEx, MENU_POSITION);
-
-        setUpToolbar();
+        setToolBar();
     }
 
-    private void setUpToolbar() {
+    private void setToolBar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.profile_toolbar);
         setSupportActionBar(toolbar);
 
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+        ImageView profileMenu = (ImageView) findViewById(R.id.iv_profile_menu);
+        profileMenu.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                Log.d(TAG, "onMenuItemClick: clicked menu item");
-                switch (item.getItemId()) {
-                    case R.id.profile_menu:
-                        Log.d(TAG, "onMenuItemClick: profile menu navigated");
-                        break;
-                }
-                return false;
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: navigate to account settings");
+                Intent settingsIntent = new Intent(ProfileActivity.this,AccountSettingActivity.class);
+                startActivity(settingsIntent);
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.profile_menu,menu);
-        return true;
-
-    }
 }
