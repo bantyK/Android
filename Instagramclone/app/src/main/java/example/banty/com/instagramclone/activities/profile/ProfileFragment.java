3cf1ec1b78c38c0bfe05b3a57b8491f6f6756afb
Banty Kumar <bantyp92@gmail.com>
2017-08-12 15:20:58 +0530

    Profile activity refactored

---
+++ Instagramclone/app/src/main/java/example/banty/com/instagramclone/activities/profile/ProfileFragment.java
@@ -0,0 +1,10 @@
+package example.banty.com.instagramclone.activities.profile;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by banty on 12/8/17.
+ */
+
+public class ProfileFragment extends Fragment {
+}
