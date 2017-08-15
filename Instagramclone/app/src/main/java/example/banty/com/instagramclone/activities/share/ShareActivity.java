368896a50bb6c9e6b3e9ebb08cda500440d0e4d9
Banty Kumar <bantyp92@gmail.com>
2017-08-15 17:45:39 +0530

    permissions updated in manifest and dynamic permission logic added in share activity

--- Instagramclone/app/src/main/java/example/banty/com/instagramclone/activities/share/ShareActivity.java
+++ Instagramclone/app/src/main/java/example/banty/com/instagramclone/activities/share/ShareActivity.java
@@ -1,17 +1,22 @@
 package example.banty.com.instagramclone.activities.share;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 import example.banty.com.instagramclone.R;
 import example.banty.com.instagramclone.activities.BaseActivity;
+import example.banty.com.instagramclone.utils.Permissions;
 
 public class ShareActivity extends BaseActivity {
 
     private static final String TAG = "ShareActivity";
     private static final int MENU_POSITION = 2;
+    private static final int VERIFY_PERMISSIONS_REQUEST = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,40 @@ public class ShareActivity extends BaseActivity {
 
         BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottom_nav_view_bar);
         setupBottomNavigationView(bottomNavigationViewEx, MENU_POSITION);
+
+        if(checkPermissionsArray(Permissions.PERMISSIONS)) {
+            // all the permissions are granted
+        } else {
+            verifyPermissions(Permissions.PERMISSIONS);
+        }
+    }
+
+    private void verifyPermissions(String[] permissions) {
+        Log.d(TAG, "verifyPermissions: verifying permissions");
+
+        ActivityCompat.requestPermissions(this,
+                permissions,
+                VERIFY_PERMISSIONS_REQUEST);
+    }
+
+    //returns true if all the permissions are granted
+    private boolean checkPermissionsArray(String[] permissions) {
+        Log.d(TAG, "checkPermissionsArray: checking permissions array");
+
+        for(String permission : permissions) {
+            if(!checkSinglePermission(permission)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //returns true if the @param permission is granted
+    private boolean checkSinglePermission(String permission) {
+        Log.d(TAG, "checkSinglePermission: checking permission : " + permission);
+
+        int permissionRequest = ActivityCompat.checkSelfPermission(this, permission);
+
+        return permissionRequest == PackageManager.PERMISSION_GRANTED;
     }
 }
---
+++ Instagramclone/app/src/main/java/example/banty/com/instagramclone/utils/Permissions.java
@@ -0,0 +1,16 @@
+package example.banty.com.instagramclone.utils;
+
+import android.Manifest;
+
+/**
+ * Created by banty on 15/8/17.
+ */
+
+public class Permissions {
+
+    public static final String [] PERMISSIONS = {
+            Manifest.permission.CAMERA,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+    };
+}
