e007aa0ae137bc7b4e09ce7a27a97a8291c8dd26
bantyK <bantyp92@gmail.com>
2017-08-20 19:20:23 +0530

    Created the next activity for sharing images

---
+++ Instagramclone/app/src/main/java/example/banty/com/instagramclone/activities/share/NextActivity.java
@@ -0,0 +1,10 @@
+package example.banty.com.instagramclone.activities.share;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by banty on 20/08/17.
+ */
+
+public class NextActivity extends AppCompatActivity {
+}
