2707ccc582ecb744396e1d4b5ce6bee1dfd05c68
bantyK <bantyp92@gmail.com>
2018-09-15 14:01:28 +0530

    resources added

--- Instantappdemo/app/src/main/java/banty/com/instantappdemo/SignInActivity.kt
+++ Instantappdemo/app/src/main/java/banty/com/instantappdemo/SignInActivity.kt
@@ -3,7 +3,7 @@ package banty.com.instantappdemo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
